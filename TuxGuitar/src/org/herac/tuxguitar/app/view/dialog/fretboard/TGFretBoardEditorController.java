e9a4e3d663712f01c9326279eccd8f5a66decc32
Alex Abdugafarov <fswork90@gmail.com>
2018-01-08 16:56:33 +0500

    Fixed changing docking place discarding show/hide component settings

--- TuxGuitar/src/org/herac/tuxguitar/app/view/dialog/fretboard/TGFretBoardEditorController.java
+++ TuxGuitar/src/org/herac/tuxguitar/app/view/dialog/fretboard/TGFretBoardEditorController.java
@@ -1,16 +1,22 @@
 package org.herac.tuxguitar.app.view.dialog.fretboard;
 
+import org.herac.tuxguitar.app.system.config.TGConfigKeys;
+import org.herac.tuxguitar.app.system.config.TGConfigManager;
 import org.herac.tuxguitar.app.view.controller.TGToggleViewController;
 import org.herac.tuxguitar.app.view.controller.TGViewContext;
+import org.herac.tuxguitar.util.TGContext;
 
 public class TGFretBoardEditorController implements TGToggleViewController {
 
 	public void toggleView(TGViewContext context) {
-		TGFretBoardEditor editor = TGFretBoardEditor.getInstance(context.getContext());
+		TGContext innerContext = context.getContext();
+		TGFretBoardEditor editor = TGFretBoardEditor.getInstance(innerContext);
 		if( editor.isVisible()){
 			editor.hideComponent();
 		} else {
 			editor.showComponent();
 		}
+		TGConfigManager tgConfig = TGConfigManager.getInstance(innerContext);
+		tgConfig.setValue(TGConfigKeys.SHOW_FRETBOARD, editor.isVisible());
 	}
 }
--- TuxGuitar/src/org/herac/tuxguitar/app/view/dialog/piano/TGPianoEditorController.java
+++ TuxGuitar/src/org/herac/tuxguitar/app/view/dialog/piano/TGPianoEditorController.java
@@ -1,16 +1,22 @@
 package org.herac.tuxguitar.app.view.dialog.piano;
 
+import org.herac.tuxguitar.app.system.config.TGConfigKeys;
+import org.herac.tuxguitar.app.system.config.TGConfigManager;
 import org.herac.tuxguitar.app.view.controller.TGToggleViewController;
 import org.herac.tuxguitar.app.view.controller.TGViewContext;
+import org.herac.tuxguitar.util.TGContext;
 
 public class TGPianoEditorController implements TGToggleViewController {
 
 	public void toggleView(TGViewContext context) {
-		TGPianoEditor editor = TGPianoEditor.getInstance(context.getContext());
+		TGContext innerContext = context.getContext();
+		TGPianoEditor editor = TGPianoEditor.getInstance(innerContext);
 		if( editor.isVisible()){
 			editor.hideComponent();
 		} else {
 			editor.showComponent();
 		}
+		TGConfigManager tgConfig = TGConfigManager.getInstance(innerContext);
+		tgConfig.setValue(TGConfigKeys.SHOW_PIANO, editor.isVisible());
 	}
 }
