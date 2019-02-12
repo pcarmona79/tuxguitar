fac7a5c1913ecc910a24331df5c0ea95ec4f74a6
b4dc0d3r <b4dc0d3r@1a24bd73-ce44-42de-bc80-594b5cd920a0>
2019-02-12 03:25:14 +0000

    more testing
    git-svn-id: https://svn.code.sf.net/p/tuxguitar-fork/code/trunk@129 1a24bd73-ce44-42de-bc80-594b5cd920a0

--- TuxGuitar/src/org/herac/tuxguitar/app/UnitTesting.java
+++ TuxGuitar/src/org/herac/tuxguitar/app/UnitTesting.java
@@ -10,21 +10,43 @@ public class UnitTesting {
 			"c","g","d","a","e","b","fis","cis",//"gis","dis","ais","eis","bis"};
 			"f","bes","ees","aes","des","ges","ces"};
 	
-	private static int[] results = {
+	private static int[] expectedResults = {
 			0, 1, 2, 3, 4, 5, 6, 7, -1, -2, -3, -4, -5, -6, -7
 	};
 	
+	private static String[] expectedNotes = {
+			"b","b","b","b","b","b","b"
+	};
+	
 	public static void main(String[] args){
+		boolean noException = true;
 		
 		try {
+			// test set key from string
 			TGNoteSpelling spelling = new TGFactory().newNoteSpelling();
 			for(int i = 0; i < tuxGuitarKeys.length; i++) {
 				int keyValue = spelling.fromString(tuxGuitarKeys[i]);
-				if (keyValue != results[i])
+				if (keyValue != expectedResults[i])
 					throw new Exception();
 			}
+			
+			//
+			int midiNote = 59; // B below middle C
+			for(int i = 0; i < tuxGuitarKeys.length; i++) {
+				int keysignature = spelling.fromString(tuxGuitarKeys[i]);
+				spelling.setSpellingFromKey(midiNote, keysignature);
+				String result = spelling.toLilyPondString();
+				if (result != expectedNotes[i])
+					throw new Exception();
+			}
+			
 		} catch (Throwable e) {
 			e.printStackTrace();
+			noException = false;
+		}
+		
+		if (noException == false) {
+			// failure
 		}
 		System.exit(0);
 	}	
