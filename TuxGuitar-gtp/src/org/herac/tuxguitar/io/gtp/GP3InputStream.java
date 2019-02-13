d1a8a76039525c5aed8acee17be9974b67647c16
b4dc0d3r <b4dc0d3r@1a24bd73-ce44-42de-bc80-594b5cd920a0>
2019-02-13 04:41:39 +0000

    fix GP3, "Morse, Steve - Flat Baroque.gp3" looks reasonable
    git-svn-id: https://svn.code.sf.net/p/tuxguitar-fork/code/trunk@135 1a24bd73-ce44-42de-bc80-594b5cd920a0

--- TuxGuitar-gtp/src/org/herac/tuxguitar/io/gtp/GP3InputStream.java
+++ TuxGuitar-gtp/src/org/herac/tuxguitar/io/gtp/GP3InputStream.java
@@ -351,7 +351,8 @@ public class GP3InputStream extends GTPInputStream {
 		if ((flags & 0x08) != 0) {
 			readNoteEffects(note.getEffect());
 		}
-		note.getSpelling().setSpellingFromKey(note.getValue(), this.keySignature);
+		int midiValue = string.getValue() + note.getValue(); 
+		note.getSpelling().setSpellingFromKey(midiValue, this.keySignature);
 		return note;
 	}
 	
