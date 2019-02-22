468d2a0d679610de31874a013a3918b1e0558489
b4dc0d3r <b4dc0d3r@1a24bd73-ce44-42de-bc80-594b5cd920a0>
2019-02-22 04:00:52 +0000

    from Helmar Gerloni - 2019-02-05 https://sourceforge.net/p/tuxguitar/bugs/77/
    git-svn-id: https://svn.code.sf.net/p/tuxguitar-fork/code/trunk@160 1a24bd73-ce44-42de-bc80-594b5cd920a0

--- TuxGuitar-gpx/src/org/herac/tuxguitar/io/gpx/GPXDocumentReader.java
+++ TuxGuitar-gpx/src/org/herac/tuxguitar/io/gpx/GPXDocumentReader.java
@@ -240,7 +240,7 @@ public class GPXDocumentReader {
 					if( repeatNode != null ){
 						masterBar.setRepeatStart(getAttributeBooleanValue(repeatNode, "start"));
 						if( getAttributeBooleanValue(repeatNode, "end") ){
-							masterBar.setRepeatCount( getAttributeIntegerValue(repeatNode, "count"));
+							masterBar.setRepeatCount( getAttributeIntegerValue(repeatNode, "count") - 1);
 						}
 					}
 					
